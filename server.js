e6fb880dc34b901d30f1c61072153b2aff238a3e
AmponsahJonathanBoadu <kkwesi956@gmail.com>
2025-05-09 09:03:34 +0000

    fix: update Swagger server URL configuration to fallback to localhost

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: process.env.URL,
+        url: process.env.URL || `http://localhost:${process.env.PORT}`,
       },
     ],
     components: {
