f018b5289b0b735e9e70e05859fcbb41951056b9
AmponsahJonathanBoadu <kkwesi956@gmail.com>
2025-08-20 16:46:28 +0000

    fix: update CORS configuration to correctly handle allowed origins

--- server.js
+++ server.js
@@ -17,11 +17,17 @@ const adminPassword = process.env.DEFAULT_ADMIN_PASSWORD || "admin123";
 
 const port = process.env.PORT || 5000;
 
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["http://localhost:5173/"];
 
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+      return callback(new Error("Not allowed by CORS"), false);
+    },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
