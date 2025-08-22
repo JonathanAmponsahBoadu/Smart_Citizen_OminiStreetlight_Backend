f0509c960d0821e8bbac8658e5f24a977252a4db
AmponsahJonathanBoadu <kkwesi956@gmail.com>
2025-08-22 11:07:11 +0000

    fix: update CORS configuration to allow specific origins

--- server.js
+++ server.js
@@ -17,11 +17,14 @@ const adminPassword = process.env.DEFAULT_ADMIN_PASSWORD || "admin123";
 
 const port = process.env.PORT || 5000;
 
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://smart-citizen-oministreetlight-backend.onrender.com",
+];
 
 app.use(
   cors({
-    origin: "*", // Allow all origins
+    origin: allowedOrigins,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
